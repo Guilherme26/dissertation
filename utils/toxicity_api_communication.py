d306d785d4d2cacb5d4adfe06af0a0afd77bef49
Guilherme <guilherme.hra26@gmail.com>
2021-10-26 08:26:17 -0300

    Perform experiments with Severe Toxicity

--- utils/toxicity_api_communication.py
+++ utils/toxicity_api_communication.py
@@ -1,18 +1,18 @@
 import logging
 
-def get_toxicity_score(service, text):
+def get_toxicity_score(service, text, toxicity_type="TOXICITY"):
     toxicity_score = None
 
     try:
         if len(text) > 0 and len(text) < 3000:
             analyze_request = {
                 'comment': {'text': text},
-                'requestedAttributes': {'TOXICITY': {}}
+                'requestedAttributes': {toxicity_type: {}}
             }
             response = service.comments().analyze(body=analyze_request).execute()
             toxicity_score = (
                 response.get("attributeScores")
-                .get("TOXICITY")
+                .get(toxicity_type)
                 .get("summaryScore")
                 .get("value")
             )
