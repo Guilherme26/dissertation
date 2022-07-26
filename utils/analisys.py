150007fea57801a52ae5cf0ed6d076fd52b44a92
luiznery <luizfelipemdnery@gmail.com>
2022-07-26 21:43:14 +0000

    Adicionando novas analises linguisticas por palavra e POS. Organizando os notebooks do liwc. Refatorando utils.

--- utils/analisys.py
+++ utils/analisys.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
 def get_word_relations(word, data):
     before = {}
     after = {}
@@ -21,3 +25,153 @@ def get_word_relations(word, data):
     after = pd.DataFrame().from_dict({'word':after.keys(),'count':after.values()})
     
     return before, after
+
+def plot_liwc_scored(sentences_liwc_counts_scored,ploting_vars,scores,plot_figsize=(16,2.8),max_one_line_plots=5,save_fig=None,comparative_behaviour=None):
+    """
+    Given the ploting vars, for each var this function plots the 
+    distribution of the toxicity scores by sentence with and, without 
+    the var or a random sample. This behaviour can be controlled by the
+    comparative_behaviour argument.
+    Args:
+        sentences_liwc_counts_scored: dataframe with the LIWC features
+        ploting_vars: list of features to plot
+        scores: list of scores to plot
+        plot_figsize: size of the figure
+        max_one_line_plots: maximum number of features to plot in one line
+        save_fig: str or None; if string (not empty), saves the figure with the given name
+        comparative_behaviour: str, float or None; if str, it can be 'same_sz_sample' 
+            (to use a random sample of same size) or 'same_sz_sample_with_var' (to use a random 
+            sample of same size taken from data which the var is present). If float, 
+            it is the size of the sample to use. If None, no comparison is made.
+    """
+
+    if len(ploting_vars)==0:
+        raise Exception('No variables to plot')
+
+    plot_dim = (
+        -1 * ( - len(ploting_vars) // max_one_line_plots), 
+        min(max_one_line_plots,len(ploting_vars))
+    )
+    # print(plot_dim)
+
+    lengend_plot_i = -1 * ( - plot_dim[1] // 2) - 1
+
+    for score in [i for i in scores if 'score' in i]:
+        fig, axes = plt.subplots(plot_dim[0], plot_dim[1], sharey=True, sharex=False, figsize=plot_figsize)
+        axes = axes.ravel()
+
+        for i,var in enumerate(ploting_vars):
+            series_present = sentences_liwc_counts_scored[sentences_liwc_counts_scored[var]>0][score]
+            present_len = len(series_present)
+
+            series_not_present = None
+            if comparative_behaviour == None:
+                series_not_present = sentences_liwc_counts_scored[sentences_liwc_counts_scored[var]==0][score]
+            elif type(comparative_behaviour) == float and comparative_behaviour >= 0 and comparative_behaviour <= 1:
+                series_not_present = sentences_liwc_counts_scored.sample(frac=comparative_behaviour)[score]
+            elif type(comparative_behaviour) == str and comparative_behaviour == 'same_sz_sample':
+                series_not_present = sentences_liwc_counts_scored.sample(n=present_len)[score]
+            elif type(comparative_behaviour) == str and comparative_behaviour == 'same_sz_sample_not_present':
+                series_not_present = sentences_liwc_counts_scored[sentences_liwc_counts_scored[var]==0].sample(n=present_len)[score]
+            else:
+                raise Exception('Comparative behaviour not recognized')
+            not_present_len = len(series_not_present)
+
+            if present_len == 0 or not_present_len == 0 :
+                raise Exception('No sentences with non-zero or zero values for %s'%var)
+                
+            if i==lengend_plot_i:
+                g2 = sns.ecdfplot(series_present,     complementary=True, ax=axes[i], legend=True, color='red')
+                g1 = sns.ecdfplot(series_not_present, complementary=True, ax=axes[i], legend=True, color='black')
+
+                new_labels = ['With','Without']
+                g2.legend(new_labels)
+                g1.legend_.set_title(None)
+                for t, l in zip(g2.legend_.texts, new_labels):
+                    t.set_text(l)
+                g2.legend_.set_frame_on(False)
+                g2.legend_.set_frame_on(False)
+                
+            else:
+                sns.ecdfplot(series_present,          complementary=True, ax=axes[i], legend=False, color='red')
+                g1 = sns.ecdfplot(series_not_present, complementary=True, ax=axes[i], legend=False, color='black')
+
+            axes[i].set(ylabel=score+' - P[X>x]',xlabel='%s score - x'%var)
+
+        fig.tight_layout()
+        if save_fig:
+            plt.savefig(filepaths['imgs']+score+save_fig)
+        plt.show()
+
+def plot_liwc_features(
+    df, ploting_vars, 
+    max_one_line_plots=5, 
+    log_scale=False, 
+    plot_figsize=(16,2.8), 
+    save_fig=None,
+    hue_order=['Black Woman','Black Man','White Woman', 'White Man'],
+    legend_labels = ['BW', 'BM','WW','WM'],
+    palette=['red','orange','grey','black']
+):
+    """
+    Plots the LIWC features in a grid of plots.
+    Args:
+        df: dataframe with the LIWC features
+        ploting_vars: list of features to plot
+        max_one_line_plots: maximum number of features to plot in one line
+        log_scale: if True, plots the features in log scale
+        plot_figsize: size of the figure
+        save_fig: str or None; if string (not empty), saves the figure with the given name
+    """
+    if len(ploting_vars)==0:
+        raise Exception('No variables to plot')
+
+    plot_dim = (
+        -1 * ( - len(ploting_vars) // max_one_line_plots), 
+        min(max_one_line_plots,len(ploting_vars))
+    )
+    # print(plot_dim)
+
+    lengend_plot_i = -1 * ( - plot_dim[1] // 2) - 1
+
+    fig, axes = plt.subplots(plot_dim[0], plot_dim[1], sharey=True, sharex=False, figsize=plot_figsize)
+    axes = axes.ravel()
+
+    for i,var in enumerate(ploting_vars): 
+        if i==lengend_plot_i:
+            g = sns.ecdfplot(
+                df, 
+                x=var, 
+                hue="group",
+                complementary=True, 
+                ax=axes[i],
+                legend=True,
+                hue_order=hue_order,
+                palette=palette
+            ) 
+            g.legend_.set_title(None)
+            for t, l in zip(g.legend_.texts, legend_labels):
+                t.set_text(l)
+            g.legend_.set_frame_on(False)
+            g.legend_.set_frame_on(False)
+        else:
+            sns.ecdfplot(
+                df, 
+                x=var, 
+                hue="group",
+                complementary=True, 
+                ax=axes[i],
+                legend=False, 
+                hue_order=hue_order,
+                palette=palette
+            )
+
+        axes[i].set(ylabel='P[X>x]',xlabel='%s score - x'%var)
+        if log_scale:
+            axes[i].set(yscale='log')
+
+    fig.tight_layout()
+    if save_fig:
+        plt.savefig(filepaths['imgs']+save_fig)
+        
+    plt.show()
--- utils/data.py
+++ utils/data.py
@@ -5,6 +5,13 @@ from urllib.parse import urlparse
 from urllib.parse import parse_qs
 
 import pandas as pd
+import glob
+
+import yaml
+try:
+    filepaths = yaml.safe_load(open("../../config/filepaths.yaml"))
+except:
+    filepaths = yaml.safe_load(open("../../../config/filepaths.yaml"))
 
 def get_subtitles_df(path_00_raw = '../data/00_raw/'):
     if path_00_raw[-1] != '/':
@@ -49,7 +56,7 @@ def get_video_id(url):
     return captured_value
 
 def get_movies_dataset(csv_path='../data/download_descriptions.csv', path_00_raw='../data/00_raw/'):
-    subs_df = get_clean_subtitles_df()
+    subs_df = get_clean_subtitles_df(path_00_raw)
     df = pd.read_csv(csv_path)
 
     df['id'] = df.url.apply(get_video_id)
@@ -59,3 +66,103 @@ def get_movies_dataset(csv_path='../data/download_descriptions.csv', path_00_raw
     df = df[df.text.notna()].reset_index(drop=True)
     df['tags_count'] = df['text'].apply(count_tags)
     return df
+
+
+# #reads data - specific to each dataset
+# def read_interview():
+#     f_or_m = lambda x: 'Woman' if x == 'f' else 'Man'
+
+#     data = pd.concat(
+#         [
+#             pd.read_csv(file) \
+#                 .assign(
+#                     file=file.split('/')[-1],
+#                     group=' '.join([file.split('/')[-1].split('-')[0],f_or_m(file.split('/')[-1].split('-')[1])]).title()
+                    
+#                 )
+#             for file in glob.glob(filepaths['interview_scored']+'*')
+#         ]
+#     )
+
+#     return data
+
+def read_coraal_buckeye():
+    buckeye_meta = pd.read_csv(filepaths['buckeye_data_description'])
+    buckeye_meta.SPEAKER = buckeye_meta.SPEAKER.str.lower()
+
+    extract_coral_gender = lambda file: file.split('_')[-3]
+    f_or_m = lambda x: 'Woman' if x == 'f' else 'Man' if x == 'm' else np.nan
+
+    get_buckeye_gender = lambda speaker: buckeye_meta[buckeye_meta.SPEAKER==speaker].iloc[0]["SPEAKER'S GENDER"]
+    
+    data = pd.concat(
+        [ #reading coraal
+            pd.read_csv(file) \
+                .assign(
+                    file=file.split('/')[-1],
+                    group= 'Black ' + f_or_m(extract_coral_gender(file))
+                )
+            for file in glob.glob(filepaths['04_coraal_scored']+'*')
+        ] + [ #reading buckeye
+            pd.read_csv(file) \
+                .assign(
+                    file=file.split('/')[-1],
+                    group= 'White ' + f_or_m(get_buckeye_gender(file.split('/')[-1]))
+                )
+            for file in glob.glob(filepaths['04_buckeye_scored']+'*')
+        ]
+    )
+    return data
+
+def read_youtube():
+    desc = pd.read_csv(filepaths['youtube_data_description'])
+    desc['id'] = desc.url.apply(lambda x: x.split('/')[-1].split('&')[0].split('=')[-1])
+
+    data = pd.concat([
+        pd.read_csv(file) \
+            .assign(
+                file=file.split('/')[-1],
+            )
+        for file in glob.glob(filepaths['04_youtube_scored']+'*')
+    ]) 
+    data = data.merge(desc[['group','id']], right_on='id', left_on='file').drop(columns=['id'])
+    return data
+
+def read_twitter():
+    data = pd.concat([
+        pd.read_csv(file) \
+            .assign(
+                file=file.split('/')[-1],
+                group = file.split('/')[-1].split('_')[0]
+            )
+        for file in glob.glob(filepaths['04_twitter_scored']+'*')
+    ]) 
+    return data
+
+def read_interview():
+    process_race_gender = lambda x: "{} {}".format(x[0].title(), process_gender(x[1]))
+    process_gender = lambda x: 'Woman' if x == 'f' else 'Man' if x == 'm' else np.nan
+
+    data = pd.concat([
+        pd.read_csv(file) \
+            .assign(
+                file=file.split('/')[-1],
+                group = process_race_gender(file.split('/')[-1].split('-')[0:2]),
+            )
+        for file in glob.glob(filepaths['04_interview_scored']+'*')
+    ]) 
+    return data
+
+def load_dataset(dataset):
+    if dataset == 'youtube':
+        data = read_youtube()
+    elif dataset == 'coraal-buckeye':
+        data = read_coraal_buckeye()
+    elif dataset == 'twitter':
+        data = read_twitter()
+    elif dataset == 'interview':
+        data = read_interview()
+    else:
+        raise Exception('Unknown dataset')
+    data = data.reset_index(drop=True)
+    return data
